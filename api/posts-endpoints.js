242f32135ce0b6015a52d6ce49e60a2d4f920eba
david <davidealvarez1@gmail.com>
2020-05-05 16:09:07 -0700

    put api/posts/:id

--- api/posts-endpoints.js
+++ api/posts-endpoints.js
@@ -30,5 +30,31 @@ router.get('/', (req,res) => {
         })
 })
 
+//PUT to api/posts/:id
+router.put('/:id', (req,res) => {
+    let id = req.params.id;
+    let updated_post = req.body;
+    if(!req.body.title || !req.body.contents){
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }else{
+        Posts.findById(id)
+        .then(post => {
+            console.log('----->', post.length)
+            if(post.length == 0){
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            }else{
+                Posts.update(id, updated_post)
+                    .then(post => {
+                        res.status(200).json(post)
+                    })
+                    .catch(error => {
+                        res.status(500).json(error)
+                    })
+            }
+            
+        })
+    }
+})
+
 
 module.exports = router;
