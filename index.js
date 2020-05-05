fb3baf94a785f91c0c016a0a700597d1e58b127f
david <davidealvarez1@gmail.com>
2020-05-05 13:26:00 -0700

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const Posts = require('./data/db.js');
+const postsRouter = require('./api/posts-endpoints.js');
+
 const server = express();
+server.use(express.json());
 
 server.use('/', (req, res) => res.send('API up and running!'));
+
+server.use('/api/posts', postsRouter);
 
 // using port 9000 for this example
 server.listen(9000, () => console.log('API running on port 9000'));
